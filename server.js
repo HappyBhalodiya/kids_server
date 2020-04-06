8d57155f09f9d36bfbc0a280fec680d055708587
HappyBhalodiya <happybhalodiya98@gmail.com>
2020-04-06 09:41:12 +0530

    push

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({'extended':'true'}));           
 app.use(bodyParser.json());        
 app.use(cors());                            
-
-mongoose.connect("mongodb://localhost:27017/kids_app");
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/kids_app')
+// mongoose.connect("mongodb://localhost:27017/kids_app");
 console.log("connected");
 
 app.post('/addAlphabets', function(req, res){alphabetscontroller.addAlphabets});
@@ -28,7 +28,9 @@ app.get('/getColors',function(req, res){colorcontroller.getColors});
 
 // app.post('/addShaps',colorcontroller.addShaps);
 // app.get('/getShaps',colorcontroller.getShaps);
-app.listen(5000);
+// app.listen(5000);
+const port = process.env.PORT || 3000;
+app.listen(port);
 module.exports = app;
 
 console.log("App listening");
