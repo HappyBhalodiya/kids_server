f1fbb82f5e882089a0e4f4b10bf40fcf9a5d0e18
HappyBhalodiya <happybhalodiya98@gmail.com>
2020-04-13 18:35:33 +0530

    change file

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(cors());
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true)
+
 
 
 mongoose.connect('mongodb://192.168.43.176:27017/kids_app').then( () => {
