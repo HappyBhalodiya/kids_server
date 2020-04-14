dd56812ea72bf2a17137392c0529b57e637546e4
HappyBhalodiya <happybhalodiya98@gmail.com>
2020-04-14 18:32:35 +0530

    init

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(cors());
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
-
+mongoose.set('useUnifiedTopology', true)
 
 
 mongoose.connect('mongodb://localhost:27017/kids_app').then( () => {
